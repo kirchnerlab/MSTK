ad08b67fdd22ac8a56c1f59195fd64669ed0c88f
Marc Kirchner <mail@marc-kirchner.de>
2012-05-22 16:42:26 +0200

    Updated the doxygen config files.
    Signed-off-by: Marc Kirchner <mail@marc-kirchner.de>

--- doc/Mainpage.h
+++ doc/Mainpage.h
@@ -2,6 +2,35 @@
 
 \mainpage MSTK: The Mass Spectrometry ToolKit
 
+\section intro Introduction
+MSTK is a collection of libraries for (mostly proteomics) mass spectrometry data
+processing.
+
+Software development in mass spectrometry/proteomics data processing requires
+substantial resources. Although a diverse set of tools have been developed
+addressing various specific data processing problems, there are still only very
+few libraries that provide generic, reusable MS code components that are
+necessary for productive software development.
+
+MSTK follows the idea that underlies the boost C++ libraries: we provide an
+open, tried and tested, high-quality, fully configurable, (close to)
+zero-dependency set of libraries. This speeds initial development, results in
+fewer bugs, reduces reinvention-of-the-wheel, and cuts long-term software
+development and maintenance costs. MSTK emphasizes on generic programming
+methods, makes use of templated interfaces, accessor functions and enable
+(partial) template specializations by the user. MSTK is developed using a
+cross-platform build environment, contains an extensive test suite and code
+coverage analysis, and comes with documentation and examples. MSTK is licensed
+under an MIT license 
+and is freely
+available for academic and commercial use.
+(with the exception of the MSTK/ngq component) 
+
+\section availability Availability
+We are currently relocating our internal MSTK development repositories to GitHub. 
+The new MSTK repository is available here:
+
+\li https://github.com/kirchnerlab/MSTK
 
 */
 
